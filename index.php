1834bae95bbfd651e0f1cd91a2363a7b9347b36c
091910386 Berenice <juarezberenice83@gmail.com>
2022-03-14 20:18:08 -0600

    Primer Screenshot del Proyecto de Gitmundo "Bere"

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang= "es">
+    <head>
+        <meta charset = "UTF-8">
+        <title> Git mundo </title>
+    </head>
+<body>
+ <font size = '14' name='Temp Sans ITC'>
+
+ <marquee> Saludos a Todos! </marquee>
+
+ </font>
+ 
+ 
+</body>
+
+
