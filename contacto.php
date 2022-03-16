e82025267f5517281c4dbb60169cdafb4b256476
091910386 Berenice <juarezberenice83@gmail.com>
2022-03-15 20:18:45 -0600

    SHAPSHOT 002 del proyecto, solo modificamos index

---
+++ contacto.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang= "es">
+    <head>
+    <link rel="stylesheet" href="style.css">
+        <meta charset = "UTF-8">
+        <title> Contacto </title>
+    </head>
+<body>
+    <center>
+   <h3>
+     Correo Electronico
+ </h3>
+<p> juarezberenice83@gmail.com</p> 
+<br>
+<br>
+<h3>
+     Numero Telefonico
+ </h3>
+ <p> 9611807382</p> 
+ <br>
+<br>
+<h3>
+     Redes Sociales
+ </h3>
+ <p> Facebook: Berenice Juárez</p> 
+ <p> Instagram: Berenice Juárez</p> 
+</center>
--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
 <html lang= "es">
     <head>
+    <link rel="stylesheet" href="style.css">
         <meta charset = "UTF-8">
         <title> Git mundo </title>
     </head>
 <body>
- <font size = '14' name='Temp Sans ITC'>
-
- <marquee> Saludos a Todos! </marquee>
-
+ <font size = '14' name='Temp Sans ITC' color="blue">
+ <marquee> Saludos a Todos TICsitos! </marquee>
  </font>
- 
- 
+ <center>
+ <h2>
+     Desarrollo Web Integral
+ </h2>
+</center>
+ <div class="info" >
+ <p>
+ Uso de múltiples técnicas y tecnologías para crear sitios con buen posicionamiento y alto rendimiento. 
+ se hace uso de tecnologías de software del lado del servidor y del cliente que involucran una combinación
+ de procesos de base de datos con el uso de un navegador web a fin de realizar determinadas tareas o mostrar información.
+ Funcionalmente, el desarrollador web, que es quien realiza esta labor, normalmente sólo se preocupa por 
+ el funcionamiento del software, es tarea del diseñador web preocuparse del aspecto final de la página y del webmaster el integrar ambas partes.
+
+ </p>
+ </div>
+
 </body>
 
 
